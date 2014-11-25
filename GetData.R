c887ee97d3a133365f86881250365870b8a9b8ef
unknown <jcharliervermeire@PASL-PIT-WK784.fnfsecure.local>
2014-11-25 14:59:48 -0500

    Added notes file.  Updated GetTrain function to be more general purpose.

--- GetData.R
+++ GetData.R
@@ -1,29 +1,15 @@
-GetTrain <- function() {
+GetTrain <- function(size = -1, start = 0) {
   # Imports competition training data downloaded from Kaggle.
   #
   # Args:
-  #   null
+  #   size: The number of rows to retrieve. -1 indicates all rows.
+  #   start: The row number to start reading.
   #
   # Returns:
   #   Training set data.
   col.classes <- c("character", "integer", "integer", rep("factor", 20))
   # read training data in batches
-  batch.size <- 100000
-  iter <- 0
-  continue <- 1
-  while (continue == 1) {
-    ans <- read.csv(gzfile("data/train.gz"), colClasses = col.classes,
-                    nrows = batch.size, skip = iter * batch.size)
-    # save batch as RData file
-    save(ans, file = paste0("data/train", iter, ".RData"))
-    # check number of rows returned
-    if (nrow(ans) < batch.size) {
-      continue <- 0
-    }
-    # update iteration
-    iter <- iter + 1
-  }
-  # load first batch set
-  ans <- load("data/train0.RData")
+  ans <- read.csv(gzfile("data/train.gz"), colClasses = col.classes,
+                  nrows = size, skip = start)
   return(ans)
 }
