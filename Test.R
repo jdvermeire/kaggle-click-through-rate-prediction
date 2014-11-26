541af2e44b62bc5c8d1c8e974548871864e76154
jdvermeire <jdvermeire@gmail.com>
2014-11-25 23:31:40 -0500

    updated test script to outline beginning process.

--- Test.R
+++ Test.R
@@ -13,8 +13,24 @@ n.input <- n.features * n.dims  # number of input nodes
 
 
 # load data from training
-train <- GetTrain(10, 0)
+train <- GetTrain(1000, 0)
 
 # update hour to drop date and convert to factor
 train$hour <- as.factor(substr(as.character(train$hour), 7, 8))
 
+# separate our variables from train
+train.id <- train$id
+train.y <- train$click
+train.x <- train[, -1:-2]
+
+# create list to hold feature info
+features <- list()
+
+# populate feature list with initial values for each feature
+for (feature.name in colnames(train.x)) {
+  features[[feature.name]] <- data.frame(key = train.x[1, feature.name], 
+                                                  t(runif(n.dims)))
+}
+
+# create layers list
+layers <- list()
