de7e4d63ef0ebd4c5d7f6e892f0942bd5dfde41c
Jeffrey Charlier-Vermeire <jdvermeire@gmail.com>
2014-11-28 13:37:47 -0500

    Added add bias parameter and epsilon contraint for feed forward

--- FeedForward.R
+++ FeedForward.R
@@ -6,17 +6,20 @@ FeedForward <- function(input, theta, activation = function(x) {x},
   #   input: A matrix of input values (n x m).
   #   theta: A matrix of weights (n[ + 1] x p).
   #   activation: An activation function (i.e. sigmoid).
+  #   add.bias: Adds bias term if true.
   #
   # Returns:
   #   A matrix of output values for the current layer.
   
   # TODO(jdvermeire): create error handling process.  Check for matrices and
   #                   conformability.
+  # set epsilon to prevent gradient issues
+  epsilon <- 0.000000001
   # add bias term
   a <- if (add.bias) {
-    rbind(rep(1, dim(input)[2]), input)
+    AddBias(input)
   } else {
     input
   }
-  activation(crossprod(theta, a))
+  ans <- pmax(pmin(activation(crossprod(theta, a)), 1 - epsilon), epsilon)
 }
