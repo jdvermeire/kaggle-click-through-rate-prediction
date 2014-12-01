965022f638d29c535386a7cc561ef9435e089474
Jeffrey Charlier-Vermeire <jdvermeire@gmail.com>
2014-12-01 13:16:44 -0500

    Added random dropout to feed forward

--- FeedForward.R
+++ FeedForward.R
@@ -1,5 +1,6 @@
 FeedForward <- function(input, theta, activation = function(x) {x}, 
-                        add.bias = TRUE) {
+                        add.bias = TRUE, use.dropout = FALSE, 
+                        dropout.fact = 0.1) {
   # Computes the output of one neural network layer.
   #
   # Agrs:
@@ -7,6 +8,8 @@ FeedForward <- function(input, theta, activation = function(x) {x},
   #   theta: A matrix of weights (n[ + 1] x p).
   #   activation: An activation function (i.e. sigmoid).
   #   add.bias: Adds bias term if true.
+  #   use.dropout: Uses random dropout against theta.
+  #   dropout.fact: Random dropout factor (probability of dropout)
   #
   # Returns:
   #   A matrix of output values for the current layer.
@@ -21,5 +24,12 @@ FeedForward <- function(input, theta, activation = function(x) {x},
   } else {
     input
   }
-  ans <- pmax(pmin(activation(crossprod(theta, a)), 1 - epsilon), epsilon)
+  # random dropout
+  b <- if (use.dropout) {
+    theta * matrix(as.numeric(runif(prod(dim(theta)), 0, 1) > dropout.fact), 
+                   nrow = dim(theta)[1], ncol = dim(theta)[2])
+  } else {
+    theta
+  }
+  ans <- pmax(pmin(activation(crossprod(b, a)), 1 - epsilon), epsilon)
 }
