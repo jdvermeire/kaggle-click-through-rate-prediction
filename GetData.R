897800d75059f66f69aab8b1630d96529875f075
Jeffrey Charlier-Vermeire <jdvermeire@gmail.com>
2014-12-01 13:48:44 -0500

    Updated GetTrain to handle batch reads with explicit column names

--- GetData.R
+++ GetData.R
@@ -1,4 +1,4 @@
-GetTrain <- function(size = -1, start = 0) {
+GetTrain <- function(size = -1, start = 1) {
   # Imports competition training data downloaded from Kaggle.
   #
   # Args:
@@ -7,9 +7,14 @@ GetTrain <- function(size = -1, start = 0) {
   #
   # Returns:
   #   Training set data.
+  col.names <- c("id", "click", "hour", "C1", "banner_pos", "site_id", 
+                 "site_domain", "site_category", "app_id", "app_domain",
+                 "app_category", "device_id", "device_ip", "device_model",
+                 "device_type", "device_conn_type", "C14", "C15", "C16", "C17",
+                 "C18", "C19", "C20", "C21")
   col.classes <- c("character", "integer", "integer", rep("factor", 20))
   # read training data in batches
-  ans <- read.csv(gzfile("data/train.gz"), colClasses = col.classes,
-                  nrows = size, skip = start)
+  ans <- read.csv(gzfile("data/train.gz"), col.names = col.names,
+                  colClasses = col.classes, nrows = size, skip = start)
   return(ans)
 }
