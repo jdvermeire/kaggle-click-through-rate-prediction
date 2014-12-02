0c1393ce88eacd158745387ea546ab841bbc2761
Jeffrey Charlier-Vermeire <jdvermeire@gmail.com>
2014-12-02 09:58:47 -0500

    Added column names to settings

--- Settings.R
+++ Settings.R
@@ -12,3 +12,12 @@ file.test <- function() {
   # Uses gzfile as a wrapper function to read the test file.
   gzfile(paste0(path.data, "test.gz"))
 }
+
+# columns
+# feature columns
+feature.cols <- c("hour", "C1", "banner_pos", "site_id", "site_domain", 
+                  "site_category", "app_id", "app_domain", "app_category",
+                  "device_model", "device_type", "device_conn_type", "C14", 
+                  "C15", "C16", "C17", "C18", "C19", "C20", "C21")
+id.col <- "id"  # id column
+y.col <- "click"  # observation column
